3fdc91aa578c76849133fd229cf07f895096e28c
manishachilgar11 <manishachilgar11@gmail.com>
2026-07-15 22:40:29 +0530

    Added Student12 Repository changes

--- FirstAPI/Repository/StuRepo.cs
+++ FirstAPI/Repository/StuRepo.cs
@@ -17,13 +17,13 @@ namespace FirstAPI.Repository
         {
             //Ado.net Code
             List<Student12> student12 = new List<Student12>();
-            SqlConnection con=new SqlConnection();
+            SqlConnection con = new SqlConnection();
             String query = "Select*From Student12";
             con.Open();
             SqlCommand cmd = new SqlCommand(query, con);
             SqlDataReader reader = cmd.ExecuteReader();
 
-            while(reader.Read())
+            while (reader.Read())
             {
                 Student12 stu = new Student12()
                 {
@@ -33,7 +33,7 @@ namespace FirstAPI.Repository
                     ISActive = Convert.ToBoolean(reader["IsActive"]),
                     Email_Id = reader["Email_Id"].ToString(),
                     Fees = Convert.ToInt32(reader["Fees"])
-                 //DefaultConnection
+                    //DefaultConnection
                 };
                 //Select*From Student12
                 student12.Add(stu);
@@ -45,34 +45,42 @@ namespace FirstAPI.Repository
             Student12 stu = null;
             SqlConnection con = new SqlConnection(_connectionString);
             String query = "Select*from student12 where ID=@id";
-            con.Open() ;
-            SqlCommand cmd=new SqlCommand(query, con);
-            SqlDataReader reader=cmd.ExecuteReader();
-            if(reader.Read())
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
             {
                 Student12 student = new Student12()
                 {
-                    Student_id= Convert.ToInt32(reader["id"]),
+                    Student_id = Convert.ToInt32(reader["id"]),
                     Student_name = reader["Name"].ToString(),
                     location = reader["location"].ToString(),
                     Email_Id = reader["Emaili_id"].ToString(),
                     Fees = Convert.ToInt32(reader["Fees"]),
                 };
             }
-         }
-        public void AddStudent1(Student12 student1)
-        {
-            SqlConnection con = new SqlConnection(_connectionString); 
-           
         }
-
-        public void DeleteStudent1(int id)
+        public void AddStudent1(Student12 Student12)
         {
+            SqlConnection con = new SqlConnection(_connectionString);
+            String query = "Insert Into Student12(id,Name,loction,Email_Id,Fees) + Values(@id,@Name,@location,@Email_Id,@Fees)";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@id", Student12.Student_id);
+            cmd.Parameters.AddWithValue("@Name", Student12.Student_name);
+            cmd.Parameters.AddWithValue("@location", Student12.location);
+            cmd.Parameters.AddWithValue("@Email_Id", Student12.Email_Id);
+            cmd.Parameters.AddWithValue("@Fees", Student12.Fees);
 
         }
         public void UpdateStudent1(Student12 student1)
         {
-            
+
+        }
+        public void DeleteStudent1(int id)
+        {
+
         }
     }
 }
